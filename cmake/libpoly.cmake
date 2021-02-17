90ba099313b10b3b4963ba82136e97a2f5730c5f
patrick96 <p.ziegler96@gmail.com>
2021-02-17 20:43:35 +0100

    cmake: Fix FindLibUV

--- cmake/libpoly.cmake
+++ cmake/libpoly.cmake
@@ -48,6 +48,8 @@ endif()
 find_package(Threads REQUIRED)
 find_package(CairoFC REQUIRED)
 
+find_package(LibUV 1.8.0 REQUIRED)
+
 if (ENABLE_ALSA)
   find_package(ALSA REQUIRED)
   set(ALSA_VERSION ${ALSA_VERSION_STRING})
@@ -99,8 +101,6 @@ endif()
 # Randr is required. Searches for randr only because we may do a version check
 find_package(Xcb ${XRANDR_VERSION} REQUIRED COMPONENTS RANDR)
 find_package(Xcb REQUIRED COMPONENTS ${XORG_EXTENSIONS})
-
-find_package(LibUV 1.10.0 REQUIRED)
 
 # FreeBSD Support
 if(CMAKE_SYSTEM_NAME STREQUAL "FreeBSD")
